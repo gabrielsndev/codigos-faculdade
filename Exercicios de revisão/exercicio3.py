91be8432f8a7f2f89f801659e1f826317855aa39
gabreisndev <gabrielsn300@gmail.com>
2024-07-03 21:32:21 -0300

    exercicio3 resolvido

--- Exercicios de revisão/exercicio3.py
+++ Exercicios de revisão/exercicio3.py
@@ -5,3 +5,31 @@
 # ● Quantidade de cupons distribuídos nesse dia;
 # ● Maior valor vendido e respectiva quantidade de cupons entregues.
 
+maior = 0 
+cupons = 0
+total = 0
+
+for i in range(200):
+    compra = float(input('Digite o valor da compra: '))
+    cupom = int(compra // 30)
+    resto = compra % 30 
+    prox_cupom = 30 - resto
+    total = total + compra
+
+    if maior < compra:
+        maior = compra
+    else: pass
+
+    if cupom == 0:
+        print('Sem cupons!')
+    else:
+        print(f'Você receberá {cupom} cupons')
+        cupons = cupons + cupom
+        
+maior_cupom = maior // 30
+
+print(f'Este foi o TOTAL vendido: R${total:.2f}')
+print(f'Esta foi a quantidade de Cupons distribuidos no dia: {cupons}')
+print(f'Este foi o maior valor: R${maior:.2f}')
+print(f'E a maior quantidade de cupons distribuidos em uma única compra foi: {int(maior_cupom)}')
+
