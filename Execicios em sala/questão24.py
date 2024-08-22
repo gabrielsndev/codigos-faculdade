56fa82a42b17eead91a48daf440bec6d5365873d
gabreisndev <gabrielsn300@gmail.com>
2024-08-22 00:09:18 -0300

    Create questão24.py

---
+++ Execicios em sala/questão24.py
@@ -0,0 +1,9 @@
+#Escreva um programa que:
+#• Crie uma matriz de ordem 20 x 4 e armazene nela as 3 notas dos 20 alunos de uma
+#turma e a média de cada um deles.
+#o As notas serão lidas e armazenadas nas 3 primeiras colunas da matriz;
+#o As médias serão calculadas e armazenadas na 4a coluna da matriz.
+#• Imprima as notas dos alunos e suas respectivas médias (no formato de matriz).
+#• Calcule e imprima a média geral da turma.
+#• Calcule e imprima o número de alunos com média superior à média geral da turma.
+
