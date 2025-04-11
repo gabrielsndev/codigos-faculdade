a13c6bbf880df5d0af83b1f97a6375d0ee95b5cf
Gabriel de Souza <gabrielsndev@gmail.com>
2025-04-11 12:03:27 -0300

    padronizando o nome dos arquivos

--- poo/lab1/Main.java
+++ poo/lab1/Main.java
@@ -7,15 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Digite o nome do produto: ");
+
 		ProdutoImportado produto = new ProdutoImportado();
-		produto.setNomeProduto(input.nextLine());
+		produto.setNomeProduto(input.nextLine()); 
+
 		System.out.println("Digite o preço do produto: ");
 		produto.setPrecoProduto(input.nextFloat());
+		
 		System.out.println("Qual a cotação atual do real? ");
 		System.out.println("A cotação atual é : " + produto.getPrecoConvertido(input.nextFloat()));
-		
-		
-		
-		
 	}
 }
