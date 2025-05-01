ab94f07116dc23ca8292152c8e453b59fd8970b5
unknown <IFPB@DSK02LAB01-SR.ad.ifpb.edu.br>
2025-04-30 23:07:55 -0300

    mais um lab

---
+++ poo/lab4/Main.java
@@ -0,0 +1,31 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // System.out.println("Digite um número em Hexadecimal ");
+        // String num = input.nextLine();
+        // int numero = Integer.parseInt(num, 16);
+        // System.out.println(numero);
+
+        System.out.println("Digite um número  ");
+        int num = Integer.parseInt(input.nextLine());
+
+        // String hexa = Integer.toHexString(num);
+
+        // System.out.println(hexa);
+
+        String bin  = Integer.toBinaryString(num);
+
+        System.out.println(bin);
+
+        input.close();
+
+
+        
+    }
+
+}
